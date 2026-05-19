a2d05dba1602d7989e91e63b24241458dd73dac2
Waleed <m.waleedbinlatif@gmail.com>
2026-05-19 21:07:13 +0500

    unknown commit

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-96fc63c4'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.lmg0vne0g9g"
+    "revision": "0.ikf3hgqnpoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
