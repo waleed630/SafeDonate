2dc195a550a4b1a5512929903d9168de612974e1
Waleed <m.waleedbinlatif@gmail.com>
2026-05-19 20:09:36 +0500

    dont know

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-96fc63c4'], (function (workbox) { 'use strict';
+define(['./workbox-96ad23dd'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -79,7 +79,7 @@ define(['./workbox-96fc63c4'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.mclplailsr8"
+    "revision": "0.lhlsv1gduj"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
